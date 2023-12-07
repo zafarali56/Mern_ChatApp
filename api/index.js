479abbea26fd028631b6824c05e552d588122dba
Zafar Ali <zafaralyjamali722@gmail.com>
2023-12-07 20:14:42 +0500

    Cookie and registration backend completed

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import User from "./models/User.js";
+import cookieParser from "cookie-parser";
 import jwt from "jsonwebtoken";
 import cors from "cors";
 
@@ -22,6 +23,7 @@ const jwtSecret = process.env.JWT_SECRET;
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
@@ -33,15 +35,38 @@ app.get("/test", (req, res) => {
   res.json("test ok");
 });
 
+app.get("/profile", (req, res) => {
+  const token = req.cookies?.token;
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, (err, userData) => {
+      if (err) throw err;
+
+      res.json(userData);
+    });
+  } else {
+    res.status(401).json("no token");
+  }
+});
+
 app.post("/register", async (req, res) => {
   const { fullName, email, password } = req.body;
 
   try {
     const createdUser = await User.create({ fullName, email, password });
-    jwt.sign({ userId: createdUser._id }, jwtSecret, {}, (err, token) => {
-      if (err) throw err;
-      res.cookie("token", token).status(201).json("Okay");
-    });
+    jwt.sign(
+      { userId: createdUser._id, email },
+      jwtSecret,
+      {},
+      (err, token) => {
+        if (err) throw err;
+        res
+          .cookie("token", token, { sameSite: "none", secure: true })
+          .status(201)
+          .json({
+            id: createdUser._id,
+          });
+      }
+    );
   } catch (err) {
     if (err) throw err;
     res.status(500).json("error");
